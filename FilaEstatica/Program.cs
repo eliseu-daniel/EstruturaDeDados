910c5adc3cb82508dc5a036080cee151055a4f58
eliseu <eliseudaniel29@gmail.com>
2024-08-19 07:53:50 -0300

    alteration dotnet 6.0 for 8.0 and create ListaEstatica

--- FilaEstatica/Program.cs
+++ FilaEstatica/Program.cs
@@ -8,6 +8,20 @@ namespace FilaEstatica
         {
             System.Console.WriteLine("Trabalhando com Fila Estatica");
             System.Console.WriteLine();
+
+            Fila Impressao = new Fila(5);
+
+            Impressao.Enfileirar("A");
+            Impressao.Enfileirar("B");
+            Impressao.Enfileirar("C");
+            Impressao.Enfileirar("D");
+            Impressao.Enfileirar("E");
+
+            Impressao.Imprimir();
+
+            Impressao.Desenfileirar();
+
+            Console.ReadKey();
         }
     }
 }
---
+++ ListaEstatica/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ListaEstatica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testando");
+            Console.ReadKey();
+        }
+    }
+}
